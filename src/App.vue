0bb5c4c81d4e714d8b4440ff3b53fc9bcb039dee
misbiheyv <mishagorkovenko1103@gmail.com>
2022-06-26 14:50:52 +1000

    footer and header

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,67 @@
 <template>
-  <div>
-    <h1>ведьмак</h1>
-    ведьмак
+  <div class="main__wrapper">
+    <my-header></my-header>
+
+    <div class="main__container _container">
+      <div></div>
+    </div>
+    <my-footer></my-footer>
   </div>
 </template>
 
 <script>
+import MyHeader from './components/Header.vue'
+import MyFooter from './components/Footer.vue'
+
 export default {
+  components: { 
+    MyHeader,
+    MyFooter,
+  },
   name: 'App',
 }
 </script>
 
-<style>
-  h1 {
-    font-weight: 600 !important;
+<style lang="scss">
+  @import './assets/colors';
+  @import './assets/mixins';
+
+  .main__wrapper {
+    background-color: $gray;
+    min-height: 100vh;
+    // height: 100%;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    .main__container {
+      // min-height: 100vh;
+      height: 100%;
+      width: 100%;
+    }
+  }
+  /** ==================ELEMENTS======================= */
+  .btn {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    padding: 16px 32px;
+    gap: 10px;
+
+    font-weight: 600;
+    line-height: 24px;
+  }
+  .transparent-btn {
+    background: rgba(236, 63, 63, 0.15);
+    border: 1px solid rgba(236, 63, 63, 0.8);
+  }
+  ._container {
+    max-width: 1440px;
+    padding: 0px 120px;
+    @include laptop { padding: 0px 64px; }
+    @include tablet { padding: 0px 16px; }
   }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,64 @@
+<template>
+    <header class="header__container">
+      <div class="header _container">
+        <div class="header__logo"></div>
+        <button class="header__btn transparent-btn btn"><p>Подключить подписку</p></button>
+      </div>
+    </header>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss">
+@import '../assets/colors';
+@import '../assets/mixins';
+
+.header__container {
+    position: absolute;
+    top: 0;
+    display: flex;
+    justify-content: center;
+    background-color: black;
+    width: 100%;
+    height: 88px;
+
+    @include tablet { height: 72px; }
+    @include phone { height: 80px; }
+
+    .header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        height: 100%;
+        width: 100%;
+
+        &__logo {
+            height: 44px;
+            width: 130px;
+            background-image: url(../assets/images/logo.png);
+            background-position: center;
+            background-size: contain;
+            background-repeat: no-repeat;
+            position: relative;
+            margin-right: 15px;
+        }
+
+        &__btn {
+            @media (max-width: 399.98px) {
+            max-width: 209px;
+            min-width: max-content;
+            padding: 16px 16px;
+            p {
+                font-size: 14px;
+                white-space: nowrap;
+            }
+            }
+        }
+    }
+}
+
+</style>
